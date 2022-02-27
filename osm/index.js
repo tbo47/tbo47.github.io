a94de9ab09ece68665aacbc2b9e14166e5ad217b
--t-bo-- <thibaut@astra.com>
2022-02-26 17:57:31 -0800

    dev

--- osm/index.js
+++ osm/index.js
@@ -1,43 +1,49 @@
-/**
- * Get the current location of the user. Will only work on https.
- * @returns { latitude, longitude }
- */
-function _getLocation() {
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => resolve(position.coords))
-        } else {
-            reject('Geolocation is not supported by this browser.')
-        }
-    })
-}
 
-async function getLocationLink() {
-    const position = await _getLocation()
-    const url = `https://www.openstreetmap.org/#map=17/${position.latitude}/${position.longitude}`
-    return url
-}
+class PoiComponent {
 
-/**
- * Query an openstreetmap server to fetch POIs
- * 
- * @param {*} bbox the rectangle where to perform the query
- * @param {*} categories of pois. Like restaurant, cafe...
- * @returns Promise<Poi[]>
- */
-function _getPois(bbox, categories) {
-    const url = 'https://overpass-api.de/api/interpreter';
+    constructor() {
+    }
 
-    let quest = '';
-    categories.forEach(c => {
-        const p = `
+    /**
+     * Get the current location of the user. Will only work on https.
+     * @returns { latitude, longitude }
+     */
+    #getLocation() {
+        return new Promise((resolve, reject) => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(position => resolve(position.coords))
+            } else {
+                reject('Geolocation is not supported by this browser.')
+            }
+        })
+    }
+
+    async getLocationLink() {
+        const position = await this.#getLocation()
+        const url = `https://www.openstreetmap.org/#map=17/${position.latitude}/${position.longitude}`
+        return url
+    }
+
+    /**
+     * Query an openstreetmap server to fetch POIs
+     * 
+     * @param {*} bbox the rectangle where to perform the query
+     * @param {*} categories of pois. Like restaurant, cafe...
+     * @returns Promise<Poi[]>
+     */
+    #getPois(bbox, categories) {
+        const url = 'https://overpass-api.de/api/interpreter';
+
+        let quest = '';
+        categories.forEach(c => {
+            const p = `
           node["amenity"="${c}"](${bbox});
           way["amenity"="${c}"](${bbox});
           relation["amenity"="${c}"](${bbox});`;
-        quest += p;
-    });
+            quest += p;
+        });
 
-    const q = `
+        const q = `
         [out:json][timeout:25];
         (
             ${quest}
@@ -46,58 +52,61 @@ function _getPois(bbox, categories) {
         >;
         out skel qt;`;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(q);
-    return new Promise(resolve => {
-        xhr.onload = function () {
-            const data = JSON.parse(this.responseText);
-            const pois = data.elements.filter(p => p.tags).map(p => {
-                p = { ...p, ...p.tags } // merge the tags object into the main one
-                delete p.tags
-                p.osm_url = `https://www.openstreetmap.org/${p.type}/${p.id}`
-                p.osm_url_edit = `https://www.openstreetmap.org/edit?${p.type}=${p.id}`
-                return p
-            })
-            resolve(pois)
-        };
-    })
-}
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(q);
+        return new Promise(resolve => {
+            xhr.onload = function () {
+                const data = JSON.parse(this.responseText);
+                const pois = data.elements.filter(p => p.tags).map(p => {
+                    p = { ...p, ...p.tags } // merge the tags object into the main one
+                    delete p.tags
+                    p.osm_url = `https://www.openstreetmap.org/${p.type}/${p.id}`
+                    p.osm_url_edit = `https://www.openstreetmap.org/edit?${p.type}=${p.id}`
+                    return p
+                })
+                resolve(pois)
+            };
+        })
+    }
 
-/**
- * 
- * @returns Promise<POI[]> restaurants and cafes
- */
-function getRestaurants() {
-    return _getPois('37.8451386,-122.300148,37.8764455,-122.271823', ['cafe', 'restaurant'])
-}
+    /**
+     * 
+     * @returns Promise<POI[]> restaurants and cafes
+     */
+    getRestaurants() {
+        return this.#getPois('37.8451386,-122.300148,37.8764455,-122.271823', ['cafe', 'restaurant'])
+    }
 
-async function getRestaurantsAroundMe() {
-    const { latitude, longitude } = await _getLocation()
-    const bbox = []
-    bbox.push(latitude - 0.01)
-    bbox.push(longitude - 0.01)
-    bbox.push(latitude + 0.01)
-    bbox.push(longitude + 0.01)
-    const pois = await _getPois(bbox.join(','), ['cafe', 'restaurant'])
-    return { pois, latitude, longitude }
+    async getRestaurantsAroundMe() {
+        const { latitude, longitude } = await this.#getLocation()
+        const bbox = []
+        bbox.push(latitude - 0.01)
+        bbox.push(longitude - 0.01)
+        bbox.push(latitude + 0.01)
+        bbox.push(longitude + 0.01)
+        const pois = await this.#getPois(bbox.join(','), ['cafe', 'restaurant'])
+        return { pois, latitude, longitude }
+    }
 }
 
 (async () => {
 
-    const { pois, latitude, longitude } = await this.getRestaurantsAroundMe()
+    const poiComponent = new PoiComponent()
+    const { pois, latitude, longitude } = await poiComponent.getRestaurantsAroundMe()
     console.log(`https://www.openstreetmap.org/#map=16/${latitude}/${longitude}`)
     console.log(pois)
+    document.getElementById('progress').style.visibility = 'hidden'
     const list = document.querySelector('ul')
     pois.forEach(poi => {
         const element = document.createElement('li')
         const a = document.createElement('a')
-        const aText = document.createTextNode(poi.name);
-        a.setAttribute('href', poi.osm_url);
-        a.appendChild(aText);
+        const aText = document.createTextNode(poi.name)
+        a.setAttribute('href', poi.osm_url)
+        a.appendChild(aText)
         element.appendChild(a)
-        list.appendChild(element);
+        list.appendChild(element)
     })
 
 })()
