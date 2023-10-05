3f7dd0cf8ad61f5949babd7965af61ff25a45510
tbo47 <thibaut.lassalle@gmail.com>
2023-10-05 10:01:41 +0000

    tweak

--- wikimedia_pic_of_the_day/index.js
+++ wikimedia_pic_of_the_day/index.js
@@ -2,7 +2,6 @@ import { wikimediaPicOfTheDay } from '../ez-opendata.js';
 
 (async () => {
     const pics = await wikimediaPicOfTheDay()
-    const h = pics.reduce((acc, pic) => acc + `<div class="pic"><a href="${pic}" />${pic}</a></div>`)
-
+    const h = pics.reduce((acc, pic) => acc + `<li class="pic"><a href="${pic}" />${pic}</a></li>`)
     document.getElementById('pics').innerHTML = h
 })();
