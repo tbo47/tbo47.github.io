531bf133a4972ba6da22296e92035143c04f4b98
tbo47 <thibaut.lassalle@gmail.com>
2023-10-05 11:54:24 +0000

    better than alert

--- wikimedia/index.js
+++ wikimedia/index.js
@@ -1,5 +1,5 @@
 import { leafletAddWikimedia, leafletInitMap } from '../ez-leaflet.js';
-import { wikidataQuery, wikimediaQuery } from '../ez-opendata.js';
+import { wikimediaQuery } from '../ez-opendata.js';
 
 const renderMap = async (map) => {
     const { _northEast, _southWest } = map.getBounds()
@@ -7,9 +7,26 @@ const renderMap = async (map) => {
         const wmedia = await wikimediaQuery(_northEast, _southWest)
         return leafletAddWikimedia(map, wmedia)
     } catch (error) {
-        alert(error.info)
+        document.getElementById('error').innerHTML = error.info
+        document.getElementById('error').style.display = 'block'
+        setTimeout(() => {
+            document.getElementById('error').style.display = 'none'
+        }, 5000)
         return []
     }
+}
+
+const getLatLng = () => {
+    const url = new URL(window.location);
+    const lat = url.searchParams.get('lat')
+    const lng = url.searchParams.get('lng')
+    return { lat, lng }
+}
+
+const setLatLng = (lat, lng) => {
+    url.searchParams.set('lat', lat)
+    url.searchParams.set('lng', lng)
+    window.location.href = url.href
 }
 
 (async () => {
