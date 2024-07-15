bc685ce5b54ec841512fb563e6fdf5dc82643929
Teebo <thibaut.lassalle@gmail.com>
2024-07-15 14:46:08 +0200

    export

--- u2.js
+++ u2.js
@@ -3,12 +3,18 @@ const fs = require('fs')
 const fetchData = async () => {
     const shops = JSON.parse(fs.readFileSync('u.json'))
     const output = []
-    console.log(shops)
+    console.log(shops.length)
     output.push(`<h2>Missing OSM website</h2>`)
     shops.map((shop) => {
         const { website, lon, lat, osm, osm_area } = shop
-        if (osm && !osm.website) {
-            output.push(`<a href="${osm.osm_url_edit}" target="osmedit">${osm.osm_url_edit}</a><br>${website} <br><br>`)
+        const osm_name = osm?.name?.toLowerCase()
+        if (!osm_name) return
+        const isU = ['super u', 'hyper u', 'u express', 'u drive', 'utile', 'station u', 'marché u'].includes(osm_name)
+        if (osm && !osm.website && !isU) console.log(osm_name)
+        if (osm && !osm.website && isU) {
+            output.push(
+                `<a href="${osm.osm_url_edit}" target="osmedit">${osm.osm_url_edit}</a><br>${osm.name}<br>${website} <br><br>`
+            )
         }
     })
     output.push(`<h2>Missing OSM poi</h2>`)
