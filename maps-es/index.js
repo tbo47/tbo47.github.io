76968628202ce44494b9f5e9ec3c46542eb40a94
Thibaut(Teebo) <thibaut@todosbuild.com>
2024-10-10 10:20:50 +0000

    maps

---
+++ maps-es/index.js
@@ -0,0 +1,9 @@
+import { Map } from './maps-es.js';
+new Map().init({
+    container: '#map',
+    style: 'osm',
+    zoom: 16,
+    center: [14.709076, -17.477656],
+    token: '',
+});
+//# sourceMappingURL=index.js.map
