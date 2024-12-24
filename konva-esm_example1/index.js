b420f868087191404e0b8f281bec74553bc359b3
tbo47 <thibaut@todosbuild.com>
2024-12-24 05:14:38 +0000

    konva-es.9.3.18

--- konva-esm_example1/index.js
+++ konva-esm_example1/index.js
@@ -1,4 +1,4 @@
-import { Konva } from 'https://tbo47.github.io/konva-es.9.3.16.js'
+import { Konva } from 'https://tbo47.github.io/konva-es.9.3.18.js'
 
 function go() {
     const stage = new Konva.Stage({
