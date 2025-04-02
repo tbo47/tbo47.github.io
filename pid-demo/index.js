8e75c6a80c1732ef2acad7566ce9a70b5d5131bf
tbo47 <thibaut.lassalle@gmail.com>
2025-04-02 11:53:44 +0000

    add pid demo

---
+++ pid-demo/index.js
@@ -0,0 +1,44 @@
+import { Konva } from '../konva-es.9.3.19.js'
+
+const PID_DATASET_URL = 'https://raw.githubusercontent.com/tbo47/open-pid-icons/refs/heads/main/open-pid-icons.json'
+
+const go = async () => {
+    const stage = new Konva.Stage({
+        container: 'container', // id of container <div>
+        width: window.innerWidth,
+        height: window.innerHeight,
+    })
+
+    const layer = new Konva.Layer()
+    stage.add(layer)
+
+    const response = await fetch(PID_DATASET_URL)
+    const data = await response.json()
+    const valves = data.valve
+    valves.forEach((item) => {
+        const icon = new Konva.Path({
+            x: Math.random() * stage.width() * 0.9,
+            y: Math.random() * stage.height() * 0.9,
+            data: item.path,
+            // fill: 'green',
+            draggable: true,
+            stroke: 'black',
+            strokeWidth: 1,
+            name: 'clouding',
+            width: 50,
+            height: 50,
+        })
+        layer.add(icon)
+    })
+    layer.draw()
+}
+
+go()
+
+// Adjust canvas size on window resize
+window.addEventListener('resize', () => {
+    const stage = Konva.stages[0]
+    stage.width(window.innerWidth)
+    stage.height(window.innerHeight)
+    go()
+})
